18bee429788372faee3faa56889881cc6b1b0f8e
javierborjas <cristman11@gmail.com>
2024-01-02 23:39:20 -0300

    Mostrando el Componente de Gastos si el presupuesto es válido

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './componets/Header'
 function App() {
 
   const [presupuesto, setPresupuesto] = useState(0);
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
 
 
   return (
@@ -11,6 +12,8 @@ function App() {
         <Header 
           presupuesto={presupuesto}
           setPresupuesto={setPresupuesto}
+          isValidPresupuesto={isValidPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
         />
       </div>
   )
