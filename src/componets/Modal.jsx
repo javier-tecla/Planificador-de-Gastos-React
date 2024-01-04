f426a9666beccdf61063585bf8fedfa54a4e0c0c
javierborjas <cristman11@gmail.com>
2024-01-03 23:21:03 -0300

    Validando los Gastos

--- src/componets/Modal.jsx
+++ src/componets/Modal.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import Mensaje from './Mensaje'
 import React from 'react'
 import CerrarBtn from '../img/cerrar.svg'
 
-const Modal = ({setModal, animarModal, setAnimarModal}) => {
+const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto}) => {
 
+    const [mensaje, setMensaje] = useState('')
     const [nombre, setNombre] = useState('')
     const [cantidad, setCantidad] = useState('')
     const [categoria, setCategoria] = useState('')
@@ -11,13 +13,26 @@ const Modal = ({setModal, animarModal, setAnimarModal}) => {
 
 
     const ocultarModal = () => {
-        
         setAnimarModal(false)
-
-
         setTimeout(() => {
             setModal(false)
-        }, 500);
+        }, 500);  
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if([ nombre, cantidad, categoria ].includes('')) {
+            setMensaje('Todos los campos son obligatorios')
+
+
+            setTimeout(() => {
+                setMensaje('')
+            }, 3000);
+            return;
+        }
+
+        guardarGasto({nombre,  cantidad,  categoria})
     }
 
   return (
@@ -30,8 +45,12 @@ const Modal = ({setModal, animarModal, setAnimarModal}) => {
                 />
             </div>
 
-            <form className={`formulario ${animarModal ? 'animar' : 'cerrar'}`}>
+            <form 
+                onSubmit={handleSubmit}
+                className={`formulario ${animarModal ? 'animar' : 'cerrar'}`}
+            >
                 <legend>Nuevo Gasto</legend>
+                {mensaje && <Mensaje tipo='error'>{mensaje}</Mensaje>}
 
 
                 <div className='campo'>
