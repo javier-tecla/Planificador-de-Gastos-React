8d0bfc1c8f26e701251bb3bd040ebe20605b3da8
javierborjas <cristman11@gmail.com>
2024-01-04 20:08:11 -0300

    Mostrando los Gastos

--- src/componets/Gasto.jsx
+++ src/componets/Gasto.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
-const Gasto = () => {
-  return (
-    <div>
-        <p>Desde Gasto.jsx</p>
-    </div>
-  )
+const Gasto = ({gasto}) => {
+    const { categoria, nombre, cantidad, id } = gasto;
+    return (
+      <div className='gasto sombra'>
+          <div className='contenido-gasto'>
+
+              <div className='descripcion-gasto'>
+                <p className='categoria'>{categoria}</p>
+                <p className='nombre-gasto'>{nombre}</p>  
+              </div>
+          </div>
+      </div>
+    )
 }
 
 export default Gasto
